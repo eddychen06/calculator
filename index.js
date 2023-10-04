4e990784223d3487e8ddfd99bcb099cc9754b3d7
Eddy <57548065+eddychen06@users.noreply.github.com>
2023-10-04 10:14:41 -0400

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,70 @@
 class Calculator {
-    constructor (number, operation) 
+    constructor (previous, current) {
+        this.previous = previous
+        this.current = current
+        this.operation = ''
+        this.result = ''
+    }
+
+    addNumber(number) {
+        this.current += number
+        document.querySelector("#current").innerHTML = this.current
+    }
+
+    addOperation(operation) {
+        this.previous = this.current + ` ${operation}`
+        document.querySelector("#previous").innerHTML = this.previous
+        document.querySelector("#current").innerHTML = ""
+        this.current = ""
+        this.operation = operation
+    }
+
+    compute() {
+        switch (this.operation) {
+            case "x":
+                this.result = parseFloat(this.previous) * parseFloat(this.current)
+                break
+            case "÷":
+                this.result = parseFloat(this.previous) / parseFloat(this.current)
+                break
+            case "+":
+                this.result = parseFloat(this.previous) + parseFloat(this.current)
+                break
+            case "-":
+                this.result = parseFloat(this.previous) - parseFloat(this.current)
+                break
+        }
+
+        document.querySelector("#previous").innerHTML = ""
+        document.querySelector("#current").innerHTML = this.result
+
+        this.current = this.result
+    }
 }
 
-function addToInput(n) {
-    input = document.getElementById("input-field").value
-    document.getElementById("input-field").value = input + n
-}
 
-function calculate() {
-    input = document.getElementById("input-field").value
-    result = eval(input)
-    document.getElementById("input-field").value = result
-}
+const numberBtns = document.querySelectorAll(".number-btn")
+const operationBtns = document.querySelectorAll(".operation-btn")
+const equalBtn = document.querySelector(".equal-btn")
+
+const previous = document.querySelector("#previous").innerHTML
+const current = document.querySelector("#current").innerHTML
+
+
+const calculator = new Calculator(previous, current)
+
+numberBtns.forEach(button => {
+    button.addEventListener("click", function () {
+        calculator.addNumber(button.innerHTML)
+    })
+})
+
+operationBtns.forEach(button => {
+    button.addEventListener("click", function () {
+        calculator.addOperation(button.innerHTML)
+    })
+})
+
+equalBtn.addEventListener("click", function () {
+    calculator.compute()
+})
